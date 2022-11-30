c0e28dda2b6da8f1121b2dbd7995ff999851535a
OlegBegen <40671741+OlegBegen@users.noreply.github.com>
2022-11-30 18:42:14 +0200

    add new files

--- js_HW06_01.js
+++
@@ -1,9 +0,0 @@
-const categoriesEl = document.querySelectorAll('li.item');
-console.log("Number of categories: " + categoriesEl.length);
-for (const categorie of categoriesEl) {
-    const categorueName = categorie.firstElementChild.textContent;
-    const categorieItemEl = categorie.querySelector('ul')
-        .querySelectorAll("li");
-    console.log("Category: " + categorueName);
-    console.log("Elements: " + categorieItemEl.length);
-}
--- js_HW06_07.js
+++
@@ -1,7 +0,0 @@
-const inputScrolEl = document.querySelector("#font-size-control");
-const textScrolEl = document.querySelector("#text");
-
-inputScrolEl.addEventListener('input', inputScrol);
-function inputScrol(event) {
-    textScrolEl.style.fontSize = event.currentTarget.value + "px";
-}
